af87ea91566f7e08667b9c07fcf849f8640eb922
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-02-24 10:42:48 +0800

    randomQuotes.js variable name change

--- scripts/randomQuotes.js
+++ scripts/randomQuotes.js
@@ -10,27 +10,26 @@ const quoteInput = document.querySelector('.add-quote-input');
 const quoteForm = document.querySelector('.add-quote-form');
 const quoteReset = document.querySelector('.reset-quotes')
 const quoteDisplay = document.querySelector('.quotes');
-let something = JSON.parse(localStorage.getItem("quotes"));
+let storedQuotes = JSON.parse(localStorage.getItem("quotes"));
 
-if (something === null) {
+if (storedQuotes === null) {
     localStorage.setItem("quotes", JSON.stringify(quotes));
-    something = JSON.parse(localStorage.getItem("quotes"));
+    storedQuotes = JSON.parse(localStorage.getItem("quotes"));
     displayQuote();
 } else {
     displayQuote();
 }
 
 function displayQuote() {
-    let index = Math.floor(Math.random() * something.length);
-    quoteDisplay.innerHTML = `"${something[index]}"`;
+    let index = Math.floor(Math.random() * storedQuotes.length);
+    quoteDisplay.innerHTML = `"${storedQuotes[index]}"`;
 }
 
 quoteForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    something.push(quoteInput.value);
-    localStorage.setItem("quotes", JSON.stringify(something));
+    storedQuotes.push(quoteInput.value);
+    localStorage.setItem("quotes", JSON.stringify(storedQuotes));
     quoteInput.value = "";
-    console.log(something)
 })
 
 quoteReset.addEventListener('click', ()=>{
